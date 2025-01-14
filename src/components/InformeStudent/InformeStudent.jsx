e998dbecdef39ef554dc6918815cc164316fd98e
ruanpedroguedes <ruanpedro9999@gmail.com>
2025-01-14 10:10:07 -0300

    estilização dos informes criados pelo adm

--- src/components/InformeStudent/InformeStudent.jsx
+++ src/components/InformeStudent/InformeStudent.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import './InformeStudent.css';
 import useAuth from '../../hooks/useAuth'; // Importa o hook useAuth
 
 const InformeStudent = () => {
